2d5c30b69badbc7bdf7cd33c0ecdbf300af520d7
pbborisov18 <pbborisov18@codingburgas.bg>
2022-10-04 11:20:40 +0300

    Initial commit

---
+++ src/Dog.java
@@ -0,0 +1,40 @@
+public class Dog {
+
+    private String name;
+    private int age;
+    private String breed;
+
+    public Dog(){
+
+    }
+
+    public Dog(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("kuche", 10, "breed");
+        System.out.println(dog.getName());
+    }
+}
